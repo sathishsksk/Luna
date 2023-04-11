34f48c78263d579cd0474a405d7af2c4e1e4eacc
Mehedi Hasan Shojib | Chishiya <yesiamshojib@gmail.com>
2023-04-12 04:20:00 +0600

    Branding

--- update.py
+++ update.py
@@ -31,9 +31,9 @@ if DATABASE_URL:
     if config_dict := db.settings.config.find_one({'_id': bot_id}):  #retrun config dict (all env vars)
         conn.close()
 
-UPSTREAM_BRANCH = 'jmdkh'
+UPSTREAM_BRANCH = 'Luna'
 
-if UPSTREAM_REPO := 'https://github.com/5hojib/Luna-Portal':
+if UPSTREAM_REPO := 'https://github.com/5hojib/Luna':
     if ospath.exists('.git'):
         srun(["rm", "-rf", ".git"])
 
